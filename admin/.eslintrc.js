0049f3af5a5e3d5bab70a54702ab12c137395ab0
jiayisheji <690405541@qq.com>
2017-03-30 19:51:32 +0800

    upload admin js eslint config

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'spaced-comment': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
