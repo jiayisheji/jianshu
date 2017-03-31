b3b6dbc1e5003b0bfe7b123acdb784b9bfb500d5
jiayisheji <690405541@qq.com>
2017-03-31 22:34:32 +0800

    upload admin doc

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,29 +1,80 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <transition :name="$router.app.pageTransition">
     <router-view></router-view>
-  </div>
+  </transition>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    created: function () {
+      console.log('app created')
+    }
+  }
 </script>
 
 <style lang="postcss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-:root {
-  --white: #fff;
-  --grey: #1e1e1d;
-  --yellow: #ffad15;
-  --offwhite: #f8f8f8;
-}
+  @import "./assets/joc.css";
+  /*right start*/
+  .slide-right-enter-active {
+    transition: all .4s ease;
+  }
+
+  .slide-right-enter {
+    opacity: 0.9;
+    transform: translate3d(100%, 0, 0);
+    -webkit-transform: translate3d(100%, 0, 0);
+  }
+
+  .slide-right-leave {
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0);
+  }
+
+  .slide-right-leave-active {
+    transition: all .4s ease;
+    opacity: .5;
+    transform: translate3d(-20%, 0, 0);
+    -webkit-transform: translate3d(-20%, 0, 0);
+  }
+  /*right end*/
+  /*left start*/
+  .ms-page.slide-left-enter-active {
+    transition: all .4s ease;
+    transform: translate3d(0%, 0, 0);
+    -webkit-transform: translate3d(0%, 0, 0);
+    z-index: 1998;
+  }
+
+  .ms-page.slide-left-enter {
+    opacity: .5;
+    transform: translate3d(-20%, 0, 0);
+    -webkit-transform: translate3d(-20%, 0, 0);
+    z-index: 1998;
+  }
+
+  .ms-page.slide-left-leave {
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0);
+  }
+
+  .ms-page.slide-left-leave-active {
+    transition: all .4s ease;
+    opacity: 0.9;
+    transform: translate3d(100%, 0, 0);
+    -webkit-transform: translate3d(100%, 0, 0);
+  }
+  /*left end*/
+  .slide-fade-enter-active {
+    transition: all .4s ease;
+  }
+
+  .slide-fade-leave-active {
+    transition: all .4s ease;
+  }
+
+  .slide-fade-enter,
+  .slide-fade-leave-active {
+    padding-left: 10px;
+    opacity: 0;
+  }
 </style>
---
+++ admin/src/pages/Page.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="page" class="g-page">
+    <top-bar></top-bar>
+    <side-bar></side-bar>
+    <main class="g-main">
+      <transition :name="$router.app.pageTransition">
+        <router-view></router-view>
+      </transition>
+    </main>
+  </div>
+</template>
+<script>
+  import topBar from '../components/topBar/topBar'
+  import sideBar from '../components/sideBar/sideBar'
+  export default {
+    components: {
+      topBar,
+      sideBar
+    },
+    created: function () {
+      console.log('page created')
+    }
+  }
+</script>
+<style lang="postcss">
+  .g-page{
+    min-height: 100%;
+    position: relative;
+    font: 14px/16px "Roboto",sans-serif;
+    background-color: #F0F3F4;
+  }
+  .g-page::before{
+    content: '';
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: url(../assets/img/bg.jpg) no-repeat center center;
+    background-size: cover;
+    will-change: transform;
+    z-index: 0;
+  }
+  .g-main{
+    margin-left: 180px;
+    padding: 66px 0 34px 0;
+    min-height: 500px;
+    position: relative;
+  }
+  .g-view{
+    padding: 8px 32px 8px 40px;
+  }
+</style>
