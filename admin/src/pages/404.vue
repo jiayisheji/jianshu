7d9a952c14c99db326c51984ee9477e842483973
jiayisheji <690405541@qq.com>
2017-03-31 22:33:20 +0800

    add 3 page

---
+++ admin/src/pages/404.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="g-not-found">
+    <h1>404</h1>
+    <h3>找不到网页</h3>
+    <p>
+      对不起，您正在寻找的页面已经找到。尝试检查URL的错误，然后按浏览器上的刷新按钮或尝试在我们的应用程序中找到其他内容。
+    </p>
+    <router-link class="btn btn-primary" :to="{'name': 'dashboard'}">Dashboard</router-link>
+  </main>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style lang="postcss">
+  .g-not-found{
+    max-width: 400px;
+    z-index: 100;
+    margin: 0 auto;
+    text-align: center;
+    padding-top: 40px;
+    & h1{
+      font-size: 170px;
+    }
+    & h3{
+      font-size: 16px;
+      font-weight: 500;
+    }
+    & p{
+
+    }
+  }
+</style>
---
+++ admin/src/pages/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <main class="g-login">
+    <h1 class="login-logo">简 书</h1>
+    <h3>欢迎来到简书后台管理系统</h3>
+    <form class="" role="form" autocomplete="off">
+      <div class="form-group">
+        <input type="text" name="username" class="form-control" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <input type="password" name="password" class="form-control" placeholder="Password">
+      </div>
+      <button type="button" class="u-btn u-btn-primary" @click="btn()">登 陆</button>
+    </form>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: '我是登陆页面'
+      }
+    },
+    methods: {
+      btn: function () {
+        console.log(this.$http())
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="postcss">
+  .g-login{
+    max-width: 400px;
+    z-index: 100;
+    margin: 0 auto;
+    padding-top: 40px;
+    & h3{
+        text-align: center;
+      font-size: 16px;
+      margin-top: 10px;
+      font-weight: 600;
+    }
+  }
+  .login-logo{
+    color: #e6e6e6;
+    font-size: 170px;
+    font-weight: 800;
+    letter-spacing: -10px;
+    margin-bottom: 0;
+    text-align: center;
+  }
+
+  .form-group{
+    margin-bottom: 15px;
+  }
+  .form-control{
+    background-color: #FFFFFF;
+    background-image: none;
+    border: 1px solid #e5e6e7;
+    border-radius: 1px;
+    color: inherit;
+    display: block;
+    padding: 6px 12px;
+    transition: border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s;
+    width: 100%;
+    font-size: 14px;
+  }
+</style>
