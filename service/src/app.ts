366e6f0fcc75fee87d057035fac0e8605519428f
jiayisheji <690405541@qq.com>
2017-06-07 23:08:09 +0800

    更新注解，防止ts编译提示报错

--- service/src/app.ts
+++ service/src/app.ts
@@ -151,7 +151,7 @@ api.index(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    let err = new Error('Not Found');
+    let err: any = new Error('Not Found');
     err.status = 404;
     next(err);
 });
