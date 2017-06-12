656842bafa3b3aa29a435786b3168d1238fa98e6
jiayisheji <690405541@qq.com>
2017-06-12 23:06:58 +0800

    添加markdown-to-html模块

--- web/src/app/article/article.component.ts
+++ web/src/app/article/article.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  private article: any;
   ngOnInit() {
+    this.article = {
+      author: {
+        avatar: "../assets/images/avatar_defaul.png",
+        _id: "593cef410f6fd7504c608846",
+        nickname: "简黛玉"
+      },
+      title: "最俗气的言情故事",
+      created: "2017-06-04 12:16:49.839Z",
+      updated: "2017-06-04 12:16:49.839Z",
+      content: '## 主命令框 \r F1 或 Ctrl+Shift+P: 打开命令面板。\r - 按一下 Backspace 会进入到 Ctrl+P 模式 \r - 在 Ctrl+P 下输入 > 可以进入 Ctrl+Shift+P 模式 \r - 在 Ctrl+P 窗口下还可以',
+      meta: {
+        wordage: 12345,
+        views_count: 100,
+        comments_count: 50,
+        likes_count: 10
+      },
+      test: '<p>test</p>',
+      books: {
+        _id: "593cef410f6fd7504c608846",
+        name: "故事"
+      }
+    }
   }
+
+
+
 }
--- web/src/app/article/article.module.ts
+++ web/src/app/article/article.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared';
 import { ArticleComponent } from './article.component';
-
+import { MarkdownToHtmlModule } from 'ng2-markdown-to-html'
 import { ROUTER_CONFIG } from './article.routes';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    ROUTER_CONFIG
+    ROUTER_CONFIG,
+    MarkdownToHtmlModule.forRoot()
   ],
   declarations: [
     ArticleComponent
