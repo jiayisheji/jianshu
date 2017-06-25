acb812e2220cbcd0b6150fb18a3ce5f44ee77dcd
jiayisheji <690405541@qq.com>
2017-06-26 00:36:34 +0800

    add dropdown

---
+++ web/src/app/core/dropdown/dropdown.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, HostListener, Output, Input, EventEmitter, Renderer, ElementRef, } from '@angular/core';
+
+@Directive({
+  selector: '[dropdown]'
+})
+export class DropdownDirective {
+  @Output() dropdownStatusChange = new EventEmitter();
+  private active: boolean = false;
+  @Input() private activeCss: string = 'open';
+
+  constructor(private elmRef: ElementRef, private renderer: Renderer) {
+    console.log('click')
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick() {
+    this.active = false;
+    this.updateHostStatus();
+  }
+
+  @HostListener('click', ['$event'])
+  onHostClick($event: Event) {
+
+    $event.stopPropagation();
+    this.active = !this.active;
+    this.updateHostStatus();
+  }
+
+  updateHostStatus() {
+    this.dropdownStatusChange.emit(this.active);
+    this.renderer.setElementClass(this.elmRef.nativeElement, this.activeCss, this.active);
+  }
+}
