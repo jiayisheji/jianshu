db4887664951b1be280b1534c429da3981682468
jiayisheji <690405541@qq.com>
2017-07-13 22:12:20 +0800

    web update 2017-07-10 完善登陆注册界面

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { LoginService } from './login.service';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {LoginService} from './login.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent implements OnInit  {
+export class LoginComponent implements OnInit {
   private error: any;
   loginForm: FormGroup;
   login = {
     username: '',
-    password: ''
+    password: '',
+    verifycode: false,
+    rememberme: true
   };
   formErrors = {
     'username': '',
@@ -21,18 +24,19 @@ export class LoginComponent implements OnInit  {
   };
   validationMessages = {
     'password': {
-      'required':      '密码不能为空',
-      'minlength':     '密码长度不能小于6位',
-      'maxlength':     '密码长度不能大于18位'
+      'required': '密码不能为空',
+      'minlength': '密码长度不能小于6位',
+      'maxlength': '密码长度不能大于18位'
     },
     'username': {
       'required': '手机号不能为空'
     }
   };
-  constructor(
-    private router: Router,
-    private fb: FormBuilder,
-    private loginService: LoginService) {}
+
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private loginService: LoginService) {
+  }
 
   ngOnInit(): void {
     this.createForm();
@@ -54,6 +58,16 @@ export class LoginComponent implements OnInit  {
           Validators.minLength(6),
           Validators.maxLength(18)
         ]
+      ],
+      verifycode: [
+        this.login.verifycode,
+        [
+          Validators.required
+        ]
+      ],
+      rememberme: [
+        this.login.rememberme,
+        []
       ]
     });
     this.loginForm.valueChanges
@@ -61,9 +75,12 @@ export class LoginComponent implements OnInit  {
   }
 
   onValueChanged(data?: any) {
-    if (!this.loginForm) { return; }
+    if (!this.loginForm) {
+      return;
+    }
     console.log('onValueChanged', this.loginForm, data);
   }
+
   loginSubmit(loginForm: any): any {
     console.log(loginForm);
     this.loginService.login(loginForm.value)
@@ -71,5 +88,11 @@ export class LoginComponent implements OnInit  {
         user => user.meta.code === 200 && this.router.navigate(['/']),
         error => this.error = error
       );
+  }
+
+
+  verify(data) {
+    this.login.verifycode = data;
+    console.log('verify', data);
   }
 }
