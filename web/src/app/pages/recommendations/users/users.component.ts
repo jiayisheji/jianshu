a997c4e897457a117276bc8a81cd627c5bc1a31d
jiayisheji <690405541@qq.com>
2017-08-28 22:46:31 +0800

    Features 完善推荐作者界面

--- web/src/app/pages/recommendations/users/users.component.ts
+++ web/src/app/pages/recommendations/users/users.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  collection = new Array(24);
   constructor() { }
 
   ngOnInit() {
