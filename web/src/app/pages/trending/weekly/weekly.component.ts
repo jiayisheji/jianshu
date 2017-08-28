d84a4b25012acdcac5acd9e21f65d2d5704d3600
jiayisheji <690405541@qq.com>
2017-08-28 22:45:17 +0800

    Features 完善7日热点界面

--- web/src/app/pages/trending/weekly/weekly.component.ts
+++ web/src/app/pages/trending/weekly/weekly.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weekly.component.css']
 })
 export class WeeklyComponent implements OnInit {
-
+  request= '/';
   constructor() { }
 
   ngOnInit() {
