b12a8fb4246ea5bcb8ffabd511d7bbdacc408f40
jiayisheji <690405541@qq.com>
2017-08-28 22:49:26 +0800

    Features 完善新上榜界面

--- web/src/app/pages/recommendations/articles/articles.component.ts
+++ web/src/app/pages/recommendations/articles/articles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-articles',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent implements OnInit {
+  request = '/';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
