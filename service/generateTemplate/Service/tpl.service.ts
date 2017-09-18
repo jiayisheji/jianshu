f19d2ec26103a1ffb25a8df9db809b25369c16ab
jiayi <690405541@qq.com>
2017-09-18 15:23:42 +0800

    Create tpl.service.ts

---
+++ service/generateTemplate/Service/tpl.service.ts
@@ -0,0 +1,27 @@
+/**
+ * {{fileName}} 服务
+ * Created by {{author}} on {{createAt}}.
+ */
+
+/**
+ * 定义服务类接口
+ */
+interface Interface{{fileName}} {
+    test();
+};
+
+/**
+ * 服务
+ */
+class {{fileName}}Service implements Interface{{fileName}} {
+    constructor() {
+    }
+
+    test() {
+    }
+}
+
+/**
+ * 导出服务模块
+ */
+export default new {{fileName}}Service();
