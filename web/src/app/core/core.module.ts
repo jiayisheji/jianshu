9de837955d8fb6fc088c106d7e1d385d01771010
jiayisheji <690405541@qq.com>
2017-10-29 18:15:22 +0800

    perf(core): update 添加LayoutModule模块 添加仿重复导入限制

--- web/src/app/core/core.module.ts
+++ web/src/app/core/core.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared';
+import { LayoutModule } from '../layout';
+
+/**
+ * 注入公共模块
+ */
+/* import {ToastModule} from './toast'; */
+
+/**
+ * 注入公共服务
+ */
+import { UsersService } from './users';
 
 @NgModule({
   imports: [
-    HttpModule
+    HttpClientModule,
+    // ToastModule,
+    LayoutModule
   ],
   providers: [
-  ],
-  declarations: [
+    UsersService
   ],
   exports: [
+    // ToastModule,
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
 
