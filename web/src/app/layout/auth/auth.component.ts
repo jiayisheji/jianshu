9369a4a2f376c0a9318d04e956d6b2a4a8707648
jiayisheji <690405541@qq.com>
2017-10-29 17:19:23 +0800

    feat(layout): add 注册登录找回密码公共组件

---
+++ web/src/app/layout/auth/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
