a3a1033bad08f26b1bbbddd87d742d70342ac154
jiayisheji <690405541@qq.com>
2017-11-21 22:01:12 +0800

    perf(共享模块):  新增UI组件模块，导入导出

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+/**
+ * 自定义UI组件
+ */
+import { SimpleModule } from '@app/simple';
+
+// imports and exports Module
+const sharedModule = [
+  CommonModule,
+  RouterModule,
+  FormsModule,
+  ReactiveFormsModule,
+  FlexLayoutModule,
+  SimpleModule
+];
+
+// declarations and exports Component
+const sharedComponent = [
+];
+
+
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: sharedModule,
   declarations: [
+    ...sharedComponent,
   ],
   exports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
+    ...sharedModule,
+    ...sharedComponent
   ]
 })
 export class SharedModule { }
