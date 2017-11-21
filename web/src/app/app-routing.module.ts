20f80249ba7d20602592c2feec56d94027b8734c
jiayisheji <690405541@qq.com>
2017-11-21 21:20:48 +0800

    refactor(app):  修改跟路由文件路径，把404和500路由改成懒加载模块形式

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from './layout';
-import { HomeComponent } from './pages/home';
-import { ErrorComponent } from './pages/error/error.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LayoutComponent } from '@app/feature/layout';
+import { HomeComponent } from '@app/feature/home';
+
 const routes: Routes = [
   {
     path: '',
@@ -11,14 +10,18 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: HomeComponent
-      }
+        component: HomeComponent,
+      },
     ]
   },
-  { path: 'register', loadChildren: 'app/pages/register/register.module#RegisterModule' },
-  { path: '404', component: NotFoundComponent },
-  { path: '500', component: ErrorComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: 'register', loadChildren: 'app/feature/auths/register/register.module#RegisterModule' },
+  { path: '404', loadChildren: 'app/feature/exception/not-found/not-found.module#NotFoundModule' },
+  { path: '500', loadChildren: 'app/feature/exception/inteal-server-error/inteal-server-error.module#IntealServerErrorModule' },
+  {
+    path: '**',
+    redirectTo: '/404',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
