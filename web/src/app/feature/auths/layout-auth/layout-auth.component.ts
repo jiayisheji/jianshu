c9a915af832a48bdc174b147541ec9c253d328d3
jiayisheji <690405541@qq.com>
2017-11-21 21:07:45 +0800

    refactor(认证模块):  认证模块通用布局组件由layout文件夹移动到feature下的auths

---
+++ web/src/app/feature/auths/layout-auth/layout-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-auth',
+  templateUrl: './layout-auth.component.html',
+  styleUrls: ['./layout-auth.component.css']
+})
+export class LayoutAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
