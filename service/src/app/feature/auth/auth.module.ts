db0018c951cbddcf8021231a05781cfbc4480d18
jiayisheji <690405541@qq.com>
2018-01-02 19:39:50 +0800

    feat(服务端认证模块):  新增认证模块字段效验配置，路由增加中间件配置

--- service/src/app/feature/auth/auth.module.ts
+++ service/src/app/feature/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module, NestModule, MiddlewaresConsumer } from '@nestjs/common';
-
+import { Module, NestModule, MiddlewaresConsumer, RequestMethod } from '@nestjs/common';
+import * as passport from 'passport';
 import { SharedModule } from '../../shared';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user';
-
+import { ValidatorMiddleware } from '../../shared';
+import { authCheckStrategy, authCheckField } from './auth-validator.config';
 @Module({
     modules: [
         SharedModule,
@@ -16,6 +17,11 @@ import { UserModule } from '../user';
 })
 export class AuthModule implements NestModule {
     configure(consumer: MiddlewaresConsumer): void {
-        consumer.apply({}).forRoutes(AuthController);
+        consumer
+            .apply(passport.authenticate('jwt', { session: false }))
+            .forRoutes({ path: '/api/v1/logout', method: RequestMethod.GET })
+            .apply(ValidatorMiddleware)
+            .with(authCheckField, authCheckStrategy)
+            .forRoutes(AuthController);
     }
 }
