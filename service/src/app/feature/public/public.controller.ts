7f38f9d40829376796404bdf3be90d694f9f9e29
jiayisheji <690405541@qq.com>
2018-01-02 19:59:07 +0800

    feat(服务端公共模块):  新增公共字段验证配置，完善发送验证码和验证昵称的服务和控制器，

--- service/src/app/feature/public/public.controller.ts
+++ service/src/app/feature/public/public.controller.ts
@@ -1,29 +1,50 @@
-import { Controller, Post, Body, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Res, UseInterceptors } from '@nestjs/common';
 import { ApiUseTags, ApiResponse } from '@nestjs/swagger';
 import { PublicService } from './public.service';
 import { SendMobileCodeDto, CheckNicknameDto } from './dto';
+import { ResponseHandler } from '../../shared';
 
+import { PassportService } from '../../shared';
 @ApiUseTags('公共模块 public')
 @Controller('api/v1')
 export class PublicController {
-    constructor(private readonly _public: PublicService) { }
+    constructor(
+        private readonly _public: PublicService,
+        private responseHandler: ResponseHandler,
+        private passportService: PassportService,
+    ) { }
     @Post('/send_mobile_code')
     @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
     @ApiResponse({ status: 403, description: 'Forbidden.' })
-    sendMobileCode(
+    async sendMobileCode(
         @Res() res,
         @Body() sendMobileCodeDto: SendMobileCodeDto,
     ) {
-        res.status(HttpStatus.CREATED).json(this._public.sendMobileCode(sendMobileCodeDto));
+        const code = await this._public.sendMobileCode(sendMobileCodeDto);
+        res.status(HttpStatus.OK).json(this.responseHandler.invoke(0, '验证码已发送'));
     }
 
     @Post('/check_nickname')
     @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
     @ApiResponse({ status: 403, description: 'Forbidden.' })
-    checkNickname(
+    async checkNickname(
         @Res() res,
         @Body() checkNicknameDto: CheckNicknameDto,
     ) {
-        res.status(HttpStatus.CREATED).json(this._public.checkNickname(checkNicknameDto));
+        const user = await this._public.checkNickname(checkNicknameDto);
+        if (user === null) {
+            res.status(HttpStatus.OK).json(this.responseHandler.invoke(0, '验证成功'));
+        }
+        res.status(HttpStatus.OK).json(this.responseHandler.invoke(1, '昵称已存在'));
+    }
+
+    @Post('/getToken')
+    @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
+    async getToken(
+        @Res() res,
+    ) {
+        const user = await this.passportService.createToken({ slug: '1' }, 60);
+        res.status(HttpStatus.OK).json(this.responseHandler.create(user));
     }
 }
--- service/src/app/feature/public/public.service.ts
+++ service/src/app/feature/public/public.service.ts
@@ -1,14 +1,25 @@
-import { Component } from '@nestjs/common';
+import { Component, Logger } from '@nestjs/common';
 import { UserService } from '../user';
 import { SendMobileCodeDto, CheckNicknameDto } from './dto';
+import { GeneratRandom, MobileCodeRedis } from '../../shared';
 @Component()
 export class PublicService {
-    constructor(private userService: UserService) { }
-    sendMobileCode(body) {
-        return { code: 123456 };
+    private readonly logger = new Logger(PublicService.name);
+    constructor(
+        private userService: UserService,
+        private generatRandom: GeneratRandom,
+        private mobileCodeRedis: MobileCodeRedis,
+    ) { }
+    async sendMobileCode(body): Promise<{ code: string } | null> {
+        const code = await this.generatRandom.size(6);
+        await this.mobileCodeRedis.saveCode(body.mobile, code);
+        this.logger.log(code);
+        return Promise.resolve({
+            code,
+        });
     }
 
-    checkNickname(nickname: CheckNicknameDto) {
-        return this.userService.findOne(nickname);
+    async checkNickname(nickname: CheckNicknameDto) {
+        return await this.userService.findOne(nickname);
     }
 }
