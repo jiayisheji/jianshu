123bfe077498b47aeaa40067d7fba76ce8d33269
jiayisheji <690405541@qq.com>
2018-01-02 19:35:53 +0800

    perf(服务端认证模块):  完善注册功能服务

--- service/src/app/feature/auth/auth.service.ts
+++ service/src/app/feature/auth/auth.service.ts
@@ -1,13 +1,59 @@
-import { Component } from '@nestjs/common';
+import { Component, Logger } from '@nestjs/common';
 import { UserService } from '../user';
+import { FormatData, PassportService, MobileCodeRedis } from '../../shared';
+import { addListener } from 'cluster';
 @Component()
 export class AuthService {
-    constructor(private userService: UserService) { }
-    login(body) {
-        return [];
+    private readonly logger = new Logger(AuthService.name);
+    constructor(
+        private userService: UserService,
+        private formatData: FormatData,
+        private passportService: PassportService,
+        private mobileCodeRedis: MobileCodeRedis,
+    ) { }
+    async login(body) {
+        return await this.userService.findOne(body);
     }
 
-    register(body) {
-        return this.userService.create(body);
+    async register(body): Promise<any> {
+        const username = await this.userService.findOne({ username: body.username });
+        if (username) {
+            return Promise.reject('手机号 已被使用，换一个吧');
+        }
+        const nickname = await this.userService.findOne({ nickname: body.nickname });
+        if (nickname) {
+            return Promise.reject('昵称 已被使用，换一个吧');
+        }
+        const code = await this.mobileCodeRedis.getCode(body.username, body.code);
+        if (!code) {
+            return Promise.reject('验证码无效或已过期，请重新发送验证码');
+        }
+        try {
+            let newUser = await this.userService.create(body);
+            if (newUser) {
+                newUser = this.formatData.UserInfo(newUser);
+            }
+            await this.mobileCodeRedis.removeCode(body.username, body.code);
+            return await this.setToken(newUser);
+        } catch (error) {
+            this.logger.log(error.errmsg);
+        }
     }
+
+    async forgetMobile(body) {
+        return await this.userService.findOne(body);
+    }
+
+    async logout(id) {
+        return await this.passportService.removeToken(id);
+    }
+
+    async setToken(newUser): Promise<any> {
+        const token = await this.passportService.createToken(newUser, 120000);
+        return Promise.resolve({
+            ...newUser,
+            ...token,
+        });
+    }
+
 }
