84a1c10a23c9249ff5cf8ee1398aba8dc9829f8d
jiayisheji <690405541@qq.com>
2018-01-02 19:21:53 +0800

    feat(服务端核心模块):  添加数据验证和API调用限制中间件及redis操作包

--- service/src/app/core/express/express.config.ts
+++ service/src/app/core/express/express.config.ts
@@ -9,7 +9,14 @@ import cors = require('cors');
 import helmet = require('helmet');
 // node.js启用gzip
 import compression = require('compression');
-
+// validator 数据验证
+import expressValidator = require('express-validator');
+// API调用限制
+import RateLimit = require('express-rate-limit');
+// API调用限制配置存储到reids
+import RedisStore = require('rate-limit-redis');
+// redis操作
+import * as Redis from 'ioredis';
 // 跨域配置
 const corsOptions = { // 处理跨域 需要提供Authorization
     origin: '*',
@@ -21,6 +28,44 @@ const corsOptions = { // 处理跨域 需要提供Authorization
 // 压缩配置
 const compressionOptions = {};
 
+// expressValidator配置
+const expressValidatorOptions = {
+    errorFormatter(param, errormsg, value) {
+        const namespace = param.split('.');
+        let formParam = namespace.shift();
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            errormsg,
+            value,
+        };
+    },
+    customValidators: {
+        isArray(value: any) {
+            return Array.isArray(value);
+        },
+        gte(param: number, num: number) {
+            return param >= num;
+        },
+    },
+};
+
+// API调用限制配置
+const rateLimitOptions = new RateLimit({
+    store: new RedisStore({  // redis存储计数
+        expiry: 60,
+        prefix: 'ratelimit',
+        client: new Redis(6379),
+    }),
+    windowMs: 60 * 1000, // 一分钟
+    delayAfter: 10,   // 10次以后开始延迟
+    delayMs: 1 * 1000, // 每次延迟1秒
+    max: 60 * 1000,  // 1分钟最大调用次数60次
+    message: '操作过于频繁，请稍等一下再试吧', // 提示信息
+});
+
 @Component()
 export class ExpressConfig {
     public configure(app: express.Application) {
@@ -31,8 +76,8 @@ export class ExpressConfig {
             .use(helmet.noCache())
             .use(compression(compressionOptions))
             .use(bodyParser.json())
-            .use(bodyParser.urlencoded({
-                extended: true,
-            }));
+            .use(bodyParser.urlencoded({ extended: true }))
+            .use(expressValidator(expressValidatorOptions))
+            .use(rateLimitOptions);
     }
 }
