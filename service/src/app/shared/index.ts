12ed9c0d33c91c3ec8b1f98a2634f1dc3d8aae5c
jiayisheji <690405541@qq.com>
2018-01-02 19:56:54 +0800

    feat(服务端共享模块):  新增mongoose，helper，middewares，passport，redis等服务

--- service/src/app/shared/index.ts
+++ service/src/app/shared/index.ts
@@ -1,2 +1,7 @@
 export * from './shared.module';
 export * from './environments';
+export * from './mongoose';
+export * from './helper';
+export * from './middlewares';
+export * from './passport';
+export * from './redis';
--- service/src/app/shared/shared.module.ts
+++ service/src/app/shared/shared.module.ts
@@ -1,15 +1,35 @@
 import { MiddlewaresConsumer, Module, RequestMethod } from '@nestjs/common';
 
 import { Environments } from './environments';
+import { MongooseService } from './mongoose';
+import {
+    GeneratRandom,
+    ResponseHandler,
+    FormatData,
+} from './helper';
+import { PassportService, JwtStrategy } from './passport';
 
+import { MobileCodeRedis } from './redis';
+
+// 共享注册组件和导出组件
+const SharedComponents = [
+    Environments,
+    MongooseService,
+    GeneratRandom,
+    ResponseHandler,
+    FormatData,
+    JwtStrategy,
+    PassportService,
+    MobileCodeRedis,
+];
 @Module({
     modules: [],
     controllers: [],
     components: [
-        Environments,
+        ...SharedComponents,
     ],
     exports: [
-        Environments,
+        ...SharedComponents,
     ],
 })
 export class SharedModule { }
