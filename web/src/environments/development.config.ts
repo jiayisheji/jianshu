1c5911fb666d10584c0b45678197857177147594
jiayi <690405541@qq.com>
2019-10-28 21:39:29 +0800

    chore:  移除旧工程

--- web/src/environments/development.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  'baseUrl': '/',
-  'api': {
-    'host': 'http://localhost:3000/api/v1'
-  }
-}
--- web/src/environments/production.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  'baseUrl': '/',
-  'env': 'prod',
-  'api': {
-    'host': 'http://localhost:3000/api/v1'
-  }
-}
