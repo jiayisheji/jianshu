21ff8c5757317991ff15daa0f5c4a07ced44bb55
jiayi <690405541@qq.com>
2019-12-06 09:00:45 +0800

    feat(utils): 添加ng style and class 类型

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/check';
 export * from './lib/merge';
 export * from './lib/list-to-tree';
 export * from './lib/tree-to-list';
+export * from './lib/types';
+export * from './lib/type';
--- libs/utils/src/lib/types.ts
+++ libs/utils/src/lib/types.ts
@@ -1,2 +1,11 @@
 // tslint:disable-next-line: no-any
 export type AnyType = any;
+export type NgClassType = string | string[] | Set<string> | NgClassInterface;
+
+export interface NgClassInterface {
+  [className: string]: boolean;
+}
+
+export interface NgStyleInterface {
+  [attr: string]: string | number;
+}
